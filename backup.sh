c80f9f9be3000ae4e78714870ae355609c7286f3
Rodrigo Cabral <rodrigo.cabral@pmm.am.gov.br>
2014-08-12 14:24:01 -0400

    novas modificações da semad para bkp

--- backup.sh
+++ backup.sh
@@ -42,29 +42,44 @@ sudo find /etc/ -name '*save' -exec rm {} \;
 #sudo find / -name '*~' -exec rm {} \;
 
 mysqldump --all-databases -u root --password=xkpptrock1990 > /opt/bkp/${origin}__mysqldump.sql
-tar -czvf /opt/bkp/${origin}__mysqldump.sql.tgz /opt/bkp/${origin}__mysqldump.sql
+#tar -czvf /opt/bkp/${origin}__mysqldump.sql.tgz /opt/bkp/${origin}__mysqldump.sql
+rar a -m5 -idp -y /opt/bkp/${origin}__mysqldump.sql.rar /opt/bkp/${origin}__mysqldump.sql
 rm /opt/bkp/${origin}__mysqldump.sql
-cp /opt/bkp/${origin}__mysqldump.sql.tgz ~/Copy/bkp
+cp /opt/bkp/${origin}__mysqldump.sql.rar ~/Copy/bkp
+
+#tar -czvf /opt/bkp/${origin}__config.tgz ~/.bitcasa* ~/.config* ~/.easytag ~/.gconf* ~/.gnupg* ~/.icon* ~/.filezilla* ~/.mozilla* ~/.opera ~/.purple ~/.Skype* ~/.skype* ~/.smestorage* ~/.SMEStorage* ~/.ssh ~/.synaptic ~/.thunderbird* ~/.bash* ~/bash* ~/.git* ~/.profile* ~/.Xmodmap* ~/.face* ~/.wgetrc ~/.local/share/applications
+rar a -m5 -idp -y /opt/bkp/${origin}__config.rar ~/.bitcasa* ~/.config* ~/.easytag ~/.gconf* ~/.gnupg* ~/.icon* ~/.filezilla* ~/.mozilla* ~/.opera ~/.purple ~/.Skype* ~/.skype* ~/.smestorage* ~/.SMEStorage* ~/.ssh ~/.synaptic ~/.thunderbird* ~/.bash* ~/bash* ~/.git* ~/.profile* ~/.Xmodmap* ~/.face* ~/.wgetrc ~/.local/share/applications
+cp /opt/bkp/${origin}__config.tgz ~/Copy/bkp
+
+#tar -czvf /opt/bkp/${origin}__etc.tgz /etc
+rar a -m5 -idp -y /opt/bkp/${origin}__etc.rar /etc
 
 #tar -czvf /opt/bkp/${origin}__bin.tgz ~/bin --exclude=$HOME/bin/adt-bundle* --exclude=$HOME/bin/AptanaStudio* --exclude=$HOME/bin/eclipse* --exclude=$HOME/bin/FreeNet* --exclude=$HOME/bin/netbeans* --exclude=$HOME/bin/smartgit* --exclude=$HOME/bin/sqldeveloper* --exclude=$HOME/bin/tor* --exclude=$HOME/bin/Zend* --exclude=$HOME/bin/cria* --exclude=$HOME/bin/bin* --exclude=$HOME/bin/t50*
 #cp /opt/bkp/${origin}__bin.tgz ~/Copy/bkp
-tar -czvf /opt/bkp/${origin}__lib.tgz ~/lib
+
+#tar -czvf /opt/bkp/${origin}__lib.tgz ~/lib
+rar a -m5 -idp -y /opt/bkp/${origin}__lib.rar ~/lib
 cp /opt/bkp/${origin}__lib.tgz ~/Copy/bkp
-tar -czvf /opt/bkp/${origin}__config.tgz ~/.bitcasa* ~/.config* ~/.easytag ~/.gconf* ~/.gnupg* ~/.icon* ~/.filezilla* ~/.mozilla* ~/.opera ~/.purple ~/.Skype* ~/.skype* ~/.smestorage* ~/.SMEStorage* ~/.ssh ~/.synaptic ~/.thunderbird* ~/.bash* ~/bash* ~/.git* ~/.profile* ~/.Xmodmap* ~/.face* ~/.wgetrc ~/.local/share/applications
-cp /opt/bkp/${origin}__config.tgz ~/Copy/bkp
-tar -czvf /opt/bkp/${origin}__etc.tgz /etc
 
 #rar a -rr -r -v1g -y /opt/bkp/${origin}_pastas.rar -x.* ~/bin/*.*sh ~/bin/*.*py ~/bin/*profile ~/bin/tor* ~/bin/sql* ~/Imagens ~/Aptana\ Studio\ 3\ Workspace/ ~/Projects ~/ProgramasRFB ~/public_html /usr/share/backgrounds /etc
 rar a -m5 -idp -y /opt/bkp/${origin}__Skype.rar ~/Skype
 cp /opt/bkp/${origin}__Skype.rar ~/Copy/bkp
+
 rar a -m5 -idp -y /opt/bkp/${origin}__projects.rar ~/projects
 cp /opt/bkp/${origin}__projects.rar ~/Copy/bkp
+
+rar a -m5 -idp -y /opt/bkp/${origin}__projects-php.rar ~/projects-php
+cp /opt/bkp/${origin}__projects-php.rar ~/Copy/bkp
+
 rar a -m5 -idp -y /opt/bkp/${origin}__projects-java.rar ~/projects-java
 cp /opt/bkp/${origin}__projects-java.rar ~/Copy/bkp
+
 rar a -m5 -idp -y /opt/bkp/${origin}__workspace.rar ~/workspace
 cp /opt/bkp/${origin}__workspace.rar ~/Copy/bkp
+
 rar a -m5 -idp -y /opt/bkp/${origin}__public_html.rar ~/public_html
 cp /opt/bkp/${origin}__public_html.rar ~/Copy/bkp
+
 cp /usr/share/backgrounds ~/Imagens
 find ~/Imagens -name '*xml' -exec rm -rfv {} \;
 find ~/Imagens -name '*contest' -exec rm -rfv {} \;
