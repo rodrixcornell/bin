1474485cb9cdacbe778aa657f2cf00533b3a21cc
Rodrigo Cabral <rodrixcornell@gmail.com>
2014-09-04 01:00:05 -0400

    configurações do proxy

---
+++ setNoProxy.sh
@@ -0,0 +1,5 @@
+# No Proxy
+export https_proxy=""
+export http_proxy=""
+export ftp_proxy=""
+export socks_proxy=""
---
+++ setProxySemad_10.1.sh
@@ -0,0 +1,5 @@
+# Proxy Semad 10.1
+export https_proxy=https://172.19.10.1:3128/
+export http_proxy=http://172.19.10.1:3128/
+export ftp_proxy=ftp://172.19.10.1:3128/
+export socks_proxy=socks://172.19.10.1:3128/
