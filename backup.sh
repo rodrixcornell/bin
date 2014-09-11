825f51b4cb3faafe9676fe8220bff82aeb4d7a06
Rodrigo Cabral <rodrigo.cabral@pmm.am.gov.br>
2014-09-11 09:44:13 -0400

    bkp para meocloud

--- backup.sh
+++ backup.sh
@@ -17,12 +17,12 @@ else
     rm -rfv /opt/bkp/*
 fi
 
-if [ ! -d ~/Copy/bkp ]; then
-    mkdir ~/Copy/bkp
-    echo "Pasta Criada em ~/Copy"
+if [ ! -d ~/MEOCloud/bkp ]; then
+    mkdir ~/MEOCloud/bkp
+    echo "Pasta Criada em ~/MEOCloud"
 else
-    echo "Pasta Já Existe em ~/Copy"
-    #rm -rfv ~/Copy/bkp/*
+    echo "Pasta Já Existe em ~/MEOCloud"
+    #rm -rfv ~/MEOCloud/bkp/*
 fi
 
 killall firefox chrome opera
@@ -48,10 +48,10 @@ sudo find / -name '*.save' -exec rm {} \;
 cd ~
 echo "Copia do Bash Histórico"
 cat .bash_history > /opt/bkp/${origin}__history.txt
-cat .bash_history > ~/Copy/bkp/${origin}__history.txt
+cat .bash_history > ~/MEOCloud/bkp/${origin}__history.txt
 echo "Copia das Variáveis de Ambiente"
 export > /opt/bkp/${origin}__export.txt
-export > ~/Copy/bkp/${origin}__export.txt
+export > ~/MEOCloud/bkp/${origin}__export.txt
 
 cd ~
 echo "Dump do MySQL ou MariaDB"
@@ -59,33 +59,33 @@ mysqldump --all-databases -u root --password=xkpptrock1990 > /opt/bkp/${origin}_
 #tar -czvf /opt/bkp/${origin}__mysqldump.sql.tgz /opt/bkp/${origin}__mysqldump.sql
 rar a -m5 -idp -y /opt/bkp/${origin}__mysqldump.sql.rar /opt/bkp/${origin}__mysqldump.sql
 rm /opt/bkp/${origin}__mysqldump.sql
-cp /opt/bkp/${origin}__mysqldump.sql.rar ~/Copy/bkp
+cp /opt/bkp/${origin}__mysqldump.sql.rar ~/MEOCloud/bkp
 
 echo "Backup Configurações Oracle Client"
 rar a -m5 -idp -y /opt/bkp/${origin}__oracle_client.rar ~/oracle_client
-cp /opt/bkp/${origin}__oracle_client.rar ~/Copy/bkp
+cp /opt/bkp/${origin}__oracle_client.rar ~/MEOCloud/bkp
 
 echo "Backup Pasta Java ~/lib"
 #tar -czvf /opt/bkp/${origin}__lib.tgz ~/lib
 rar a -m5 -idp -y /opt/bkp/${origin}__lib.rar ~/lib
-cp /opt/bkp/${origin}__lib.tgz ~/Copy/bkp
+cp /opt/bkp/${origin}__lib.tgz ~/MEOCloud/bkp
 
 echo "Backup Pasta Skype"
 #rar a -rr -r -v1g -y /opt/bkp/${origin}_pastas.rar -x.* ~/bin/*.*sh ~/bin/*.*py ~/bin/*profile ~/bin/tor* ~/bin/sql* ~/Imagens ~/Aptana\ Studio\ 3\ Workspace/ ~/Projects ~/ProgramasRFB ~/public_html /usr/share/backgrounds /etc
 rar a -m5 -idp -y /opt/bkp/${origin}__Skype.rar ~/Skype
-cp /opt/bkp/${origin}__Skype.rar ~/Copy/bkp
+cp /opt/bkp/${origin}__Skype.rar ~/MEOCloud/bkp
 
 echo "Backup Pasta projetos"
 rar a -m5 -idp -y /opt/bkp/${origin}__projects.rar ~/projects*
-cp /opt/bkp/${origin}__projects.rar ~/Copy/bkp
+cp /opt/bkp/${origin}__projects.rar ~/MEOCloud/bkp
 
 echo "Backup Pasta workspace"
 rar a -m5 -idp -y /opt/bkp/${origin}__workspace.rar ~/workspace*
-cp /opt/bkp/${origin}__workspace.rar ~/Copy/bkp
+cp /opt/bkp/${origin}__workspace.rar ~/MEOCloud/bkp
 
 echo "Backup Pasta public_html"
 rar a -m5 -idp -y /opt/bkp/${origin}__public_html.rar ~/public_html
-cp /opt/bkp/${origin}__public_html.rar ~/Copy/bkp
+cp /opt/bkp/${origin}__public_html.rar ~/MEOCloud/bkp
 
 echo "Backup Pasta Pessoais"
 cp /usr/share/backgrounds ~/Imagens
@@ -108,11 +108,11 @@ rar a -m5 -idp -y /opt/bkp/${origin}__etc.rar /etc
 echo "Backup Configurações"
 tar -czvf /opt/bkp/${origin}__config.tgz ~/.bitcasa* ~/.config* ~/.easytag* ~/.filezilla* ~/.fonts* ~/.gconf* ~/.gkrellm* ~/.gnome2/nautilus-scripts* ~/.gnupg* ~/.icon* ~/.local/share/applications* ~/.mozilla* ~/.purple* ~/.receitanet* ~/.Skype* ~/.skype* ~/.*torage* ~/.ssh* ~/.synaptic* ~/.themes* ~/.thunderbird* ~/.xchat2* ~/.youtube-dl-gui* ~/.bash* ~/bash* ~/.git* ~/.inputrc* ~/.pearrc* ~/.profile* ~/.sqlplus* ~/.Xmodmap* ~/.face* ~/.nanorc* ~/.wgetrc*
 #rar a -m5 -idp -y /opt/bkp/${origin}__config.rar ~/.bitcasa* ~/.config* ~/.easytag ~/.gconf* ~/.gnupg* ~/.icon* ~/.filezilla* ~/.mozilla* ~/.opera ~/.purple ~/.Skype* ~/.skype* ~/.smestorage* ~/.SMEStorage* ~/.ssh ~/.synaptic ~/.thunderbird* ~/.bash* ~/bash* ~/.git* ~/.profile* ~/.Xmodmap* ~/.face* ~/.wgetrc ~/.local/share/applications
-cp /opt/bkp/${origin}__config.tgz ~/Copy/bkp
+cp /opt/bkp/${origin}__config.tgz ~/MEOCloud/bkp
 
 #echo "Backup Pasta ~/bin"
 #tar -czvf /opt/bkp/${origin}__bin.tgz ~/bin --exclude=$HOME/bin/adt-bundle* --exclude=$HOME/bin/AptanaStudio* --exclude=$HOME/bin/eclipse* --exclude=$HOME/bin/FreeNet* --exclude=$HOME/bin/netbeans* --exclude=$HOME/bin/smartgit* --exclude=$HOME/bin/sqldeveloper* --exclude=$HOME/bin/tor* --exclude=$HOME/bin/Zend* --exclude=$HOME/bin/cria* --exclude=$HOME/bin/bin* --exclude=$HOME/bin/t50*
-#cp /opt/bkp/${origin}__bin.tgz ~/Copy/bkp
+#cp /opt/bkp/${origin}__bin.tgz ~/MEOCloud/bkp
 
 echo
 #nohup dropbox start -i &
