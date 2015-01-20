e6685351a1c3ad95fa427833cfa4b5253246cabe
Rodrigo Cabral <rodrixcornell@gmail.com>
2015-01-20 00:01:48 -0400

    modificado: server.sh

--- server.sh
+++ server.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-if [ "$1" = "" ] then
+if [ "$1" = "" ]; then
 	host = "0.0.0.0"
 else
 	host = $1
 fi
 
-if [ "$2" = "80" ] then
+if [ "$2" = "80" ]; then
 	port = "80"
 else
 	port = $2
 fi
 
-if [ "$3" = "$HOME/public_html" ] then
+if [ "$3" = "$HOME/public_html" ]; then
 	docroot = "$HOME/public_html"
 else
 	docroot = $3
