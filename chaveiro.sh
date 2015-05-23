b0da6514b94d488a6fce125fd3c9a293a12491bb
Rodrigo Cabral <rodrixcornell@gmail.com>
2015-05-22 21:40:32 -0400

    novo pc

--- chaveiro.sh
+++ chaveiro.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gpg --keyserver pgpkeys.mit.edu --recv-key $1
-gpg -a --export $1 | sudo apt-key add -
+sudo gpg --keyserver pgpkeys.mit.edu --recv-key $1
+sudo gpg -a --export $1 | sudo apt-key add -
--- chaveiro2.sh
+++ chaveiro2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gpg --keyserver subkeys.pgp.net --recv-key $1
-gpg -a --export $1 | sudo apt-key add -
+sudo gpg --keyserver subkeys.pgp.net --recv-key $1
+sudo gpg -a --export $1 | sudo apt-key add -
