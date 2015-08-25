46c76131d788e55fe3fe72385709c593b61efb22
Rodrigo Cabral <rodrigo.cabral@pmm.am.gov.br>
2015-08-25 18:30:21 -0400

    atualizações

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -68,7 +68,7 @@ alias ati_adapters='aticonfig --lsa'
 #      Enable/disable display switching with ACPI methods on mobile platforms. This option is enabled by default.
 alias thermal2='echo; sensors; hddtemp /dev/sda; echo; aticonfig --odgt'
 alias thermal='echo; sensors; hddtemp /dev/sda; echo'
-alias nano='nano -cw'
+alias nano='nano -cimw'
 alias nano_su='sudo nano -c'
 alias find_su='sudo find'
 
