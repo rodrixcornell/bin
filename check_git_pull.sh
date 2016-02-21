1e0bec3f5e3c917950dd202acb1f8add37625339
Rodrigo Cabral <rodrixcornell@gmail.com>
2016-02-20 21:24:23 -0400

    updates

--- check_git_pull.sh
+++ check_git_pull.sh
@@ -10,113 +10,113 @@ cd ~
 echo
 cd ~/public_html/semad/contaspublicas
 echo contaspublicas
-git checkout develop && git pull && git checkout master && git pull && git checkout develop
+git checkout -f develop && git pull && git checkout -f master && git pull && git checkout -f develop
 
 echo
 cd ~/public_html/semad/contratos
 echo contratos
-git checkout develop && git pull && git checkout master && git pull && git checkout develop
+git checkout -f develop && git pull && git checkout -f master && git pull && git checkout -f develop
 
 echo
 cd ~/public_html/semad/espi
 echo espi
-git checkout develop && git pull && git checkout master && git pull && git checkout develop
+git checkout -f develop && git pull && git checkout -f master && git pull && git checkout -f develop
 
 echo
 cd ~/public_html/semad/estagio
 echo estagio
-git checkout develop && git pull && git checkout master && git pull && git checkout develop
+git checkout -f develop && git pull && git checkout -f master && git pull && git checkout -f develop
 
 echo
 cd ~/public_html/semad/estoque
 echo estoque
-git checkout develop && git pull && git checkout master && git pull && git checkout develop
-git checkout develop_tela && git pull && git checkout master && git pull && git checkout develop_tela
+git checkout -f develop && git pull && git checkout -f master && git pull && git checkout -f develop
+#git checkout -f develop_tela && git pull && git checkout -f master && git pull && git checkout -f develop_tela
 
 echo
 cd ~/public_html/semad/gestaodoc
 echo gestaodoc
-git checkout develop && git pull && git checkout master && git pull && git checkout develop
+git checkout -f develop && git pull && git checkout -f master && git pull && git checkout -f develop
 
 echo
 cd ~/public_html/semad/gestaonfe
 echo gestaonfe
-git checkout develop && git pull && git checkout master && git pull && git checkout develop
+git checkout -f develop && git pull && git checkout -f master && git pull && git checkout -f develop
 
 echo
 cd ~/public_html/semad/imoveis
 echo imoveis
-git checkout develop && git pull && git checkout master && git pull && git checkout develop
+git checkout -f develop && git pull && git checkout -f master && git pull && git checkout -f develop
 
 echo
 cd ~/public_html/semad/irp
 echo irp
-git checkout develop && git pull && git checkout master && git pull && git checkout develop
+git checkout -f develop && git pull && git checkout -f master && git pull && git checkout -f develop
 
 echo
 cd ~/public_html/semad/juridico
 echo juridico
-git checkout develop && git pull && git checkout master && git pull && git checkout develop
+git checkout -f develop && git pull && git checkout -f master && git pull && git checkout -f develop
 
 echo
 cd ~/public_html/semad/novosge
 echo novosge
-git checkout develop && git pull && git checkout master && git pull && git checkout develop
+git checkout -f develop && git pull && git checkout -f master && git pull && git checkout -f develop
 
 echo
 cd ~/public_html/semad/organizacional
 echo organizacional
-git checkout develop && git pull && git checkout master && git pull && git checkout develop
+git checkout -f develop && git pull && git checkout -f master && git pull && git checkout -f develop
 
 echo
 cd ~/public_html/semad/patrimonio
 echo patrimonio
-git checkout develop && git pull && git checkout master && git pull && git checkout develop
+git checkout -f develop && git pull && git checkout -f master && git pull && git checkout -f develop
 
 echo
 cd ~/public_html/semad/ponto
 echo ponto
-git checkout develop && git pull && git checkout master && git pull && git checkout develop
+git checkout -f develop && git pull && git checkout -f master && git pull && git checkout -f develop
 
 echo
 cd ~/public_html/semad/ponto-e
 echo ponto-e
-git checkout develop && git pull && git checkout master && git pull && git checkout develop
+git checkout -f develop && git pull && git checkout -f master && git pull && git checkout -f develop
 
 echo
 cd ~/public_html/semad/recebimento
 echo recebimento
-git checkout develop && git pull && git checkout master && git pull && git checkout develop
+git checkout -f develop && git pull && git checkout -f master && git pull && git checkout -f develop
 
 echo
 cd ~/public_html/semad/sbpg
 echo sbpg
-git checkout develop && git pull && git checkout master && git pull && git checkout develop
+git checkout -f develop && git pull && git checkout -f master && git pull && git checkout -f develop
 
 echo
 cd ~/public_html/semad/sbpg_bolsista_public
 echo sbpg_bolsista_public
-git checkout develop && git pull && git checkout master && git pull && git checkout develop
+git checkout -f develop && git pull && git checkout -f master && git pull && git checkout -f develop
 
 echo
 cd ~/public_html/semad/sbpg_ies_public
 echo sbpg_ies_public
-git checkout develop && git pull && git checkout master && git pull && git checkout develop
+git checkout -f develop && git pull && git checkout -f master && git pull && git checkout -f develop
 
 echo
 cd ~/public_html/semad/sbpg_public
 echo sbpg_public
-git checkout develop && git pull && git checkout master && git pull && git checkout develop
+git checkout -f develop && git pull && git checkout -f master && git pull && git checkout -f develop
 
 echo
 cd ~/public_html/semad/sishab
 echo sishab
-git checkout develop && git pull && git checkout master && git pull && git checkout develop
+git checkout -f develop && git pull && git checkout -f master && git pull && git checkout -f develop
 
 echo
 cd ~/public_html/semad/voyage
 echo voyage
-git checkout develop && git pull && git checkout master && git pull && git checkout develop
+git checkout -f develop && git pull && git checkout -f master && git pull && git checkout -f develop
 
 echo
 cd ~
