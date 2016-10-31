e34a5ce410eb8752ee2041f5d19097035fb4f041
Rodrigo Cabral <rodrixcornell@gmail.com>
2016-10-31 09:05:50 -0400

    Atualização Atualização dos mirros de projetos

--- git_fetch.sh
+++ git_fetch.sh
@@ -1,52 +1,58 @@
 #!/bin/bash
 
 echo 'Git Fetch All!!!'
-cd ${HOME}/projetos/repos/vo/contaspublicas.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/contratos.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/espi.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/estagio.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/estoque.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/folha.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/gestaodoc.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/gestaonfe.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/imoveis.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/irp.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/juridico.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/nfe.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/novosge.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/organizacional.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/patrimonio.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/pbpg_ies_public.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/pbpg_public.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/pbpg.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/ponto-e.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/ponto.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/recebimento.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/redmine_cli.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/sbpg_bolsista_public.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/sbpg_ies_public.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/sbpg_public.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/sbpg.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/sge.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/semad2010.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/semad.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/siscfg.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/sishab.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/sme.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/vo/voyage.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/contaspublicas.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/contratos.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/espi.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/estagio.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/estoque.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/folha.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/gestaodoc.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/gestaonfe.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/imoveis.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/irp.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/juridico.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/nfe.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/novosge.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/organizacional.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/patrimonio.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/pbpg_ies_public.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/pbpg_public.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/pbpg.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/ponto-e.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/ponto.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/recebimento.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/redmine_cli.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/sbpg_bolsista_public.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/sbpg_ies_public.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/sbpg_public.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/sbpg.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/sge.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/semad2010.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/semad.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/siscfg.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/sishab.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/sme.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/voyage.git && pwd && git fetch --all
 
 echo 'Git Fetch All Thupan!!!'
-cd ${HOME}/projetos/repos/thupan/administrativo.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/thupan/chamados.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/thupan/contratos.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/thupan/estagio.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/thupan/pmm-tube.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/thupan/ponto.git && pwd && git fetch --all
-cd ${HOME}/projetos/repos/thupan/webserve.git && pwd && git fetch --all
+cd ${HOME}/repos/thupan/administrativo.git && pwd && git fetch --all
+cd ${HOME}/repos/thupan/chamados.git && pwd && git fetch --all
+cd ${HOME}/repos/thupan/contratos.git && pwd && git fetch --all
+cd ${HOME}/repos/thupan/estagio.git && pwd && git fetch --all
+cd ${HOME}/repos/thupan/pmm-tube.git && pwd && git fetch --all
+cd ${HOME}/repos/thupan/ponto.git && pwd && git fetch --all
+cd ${HOME}/repos/thupan/webserve.git && pwd && git fetch --all
 
-echo 'Git Fetch All Thupan!!!'
-cd ${HOME}/projetos/repos/third-party-systems/semad_wpress.git && pwd && git fetch --all
+echo 'Git Fetch All Third Party!!!'
+cd ${HOME}/repos/third-party-systems/semad_wpress.git && pwd && git fetch --all
 
-echo 'Git Fetch All Thupan!!!'
-cd ${HOME}/projetos/repos/rubyonrails/redmine.git && pwd && git fetch --all
+echo 'Git Fetch All RubyOnRails!!!'
+cd ${HOME}/repos/rubyonrails/redmine.git && pwd && git fetch --all
+
+echo 'Git Fetch All Delphi!!!'
+cd ${HOME}/repos/desktop/contaspublicas.git && pwd && git fetch --all
+cd ${HOME}/repos/desktop/gestaocontaspublicas.git && pwd && git fetch --all
+cd ${HOME}/repos/desktop/habitacao.git && pwd && git fetch --all
+cd ${HOME}/repos/desktop/pontoquiosquev2.git && pwd && git fetch --all
 
