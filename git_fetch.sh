00c192c1173f9d347fba1d4be1e2d080c8fe315d
Rodrigo Cabral <rodrixcornell@gmail.com>
2016-12-07 11:13:15 -0400

    atualizações
    Signed-off-by: Rodrigo Cabral <rodrixcornell@gmail.com>

--- git_fetch.sh
+++ git_fetch.sh
@@ -16,15 +16,12 @@ cd ${HOME}/repos/vo/nfe.git && pwd && git fetch --all
 cd ${HOME}/repos/vo/novosge.git && pwd && git fetch --all
 cd ${HOME}/repos/vo/organizacional.git && pwd && git fetch --all
 cd ${HOME}/repos/vo/patrimonio.git && pwd && git fetch --all
-cd ${HOME}/repos/vo/pbpg_ies_public.git && pwd && git fetch --all
-cd ${HOME}/repos/vo/pbpg_public.git && pwd && git fetch --all
-cd ${HOME}/repos/vo/pbpg.git && pwd && git fetch --all
 cd ${HOME}/repos/vo/ponto-e.git && pwd && git fetch --all
 cd ${HOME}/repos/vo/ponto.git && pwd && git fetch --all
 cd ${HOME}/repos/vo/recebimento.git && pwd && git fetch --all
 cd ${HOME}/repos/vo/redmine_cli.git && pwd && git fetch --all
-cd ${HOME}/repos/vo/sbpg_bolsista_public.git && pwd && git fetch --all
-cd ${HOME}/repos/vo/sbpg_ies_public.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/sbpg_bolsista.git && pwd && git fetch --all
+cd ${HOME}/repos/vo/sbpg_ies.git && pwd && git fetch --all
 cd ${HOME}/repos/vo/sbpg_public.git && pwd && git fetch --all
 cd ${HOME}/repos/vo/sbpg.git && pwd && git fetch --all
 cd ${HOME}/repos/vo/sge.git && pwd && git fetch --all
@@ -42,17 +39,19 @@ cd ${HOME}/repos/thupan/contratos.git && pwd && git fetch --all
 cd ${HOME}/repos/thupan/estagio.git && pwd && git fetch --all
 cd ${HOME}/repos/thupan/pmm-tube.git && pwd && git fetch --all
 cd ${HOME}/repos/thupan/ponto.git && pwd && git fetch --all
+cd ${HOME}/repos/thupan/sac.git && pwd && git fetch --all
 cd ${HOME}/repos/thupan/webserve.git && pwd && git fetch --all
 
 echo 'Git Fetch All Third Party!!!'
-cd ${HOME}/repos/third-party-systems/semad_wpress.git && pwd && git fetch --all
+cd ${HOME}/repos/tps/semad_wpress.git && pwd && git fetch --all
+cd ${HOME}/repos/tps/semad2_wpress.git && pwd && git fetch --all
 
 echo 'Git Fetch All RubyOnRails!!!'
-cd ${HOME}/repos/rubyonrails/redmine.git && pwd && git fetch --all
+cd ${HOME}/repos/ror/redmine.git && pwd && git fetch --all
 
 echo 'Git Fetch All Delphi!!!'
-cd ${HOME}/repos/desktop/contaspublicas.git && pwd && git fetch --all
-cd ${HOME}/repos/desktop/gestaocontaspublicas.git && pwd && git fetch --all
-cd ${HOME}/repos/desktop/habitacao.git && pwd && git fetch --all
-cd ${HOME}/repos/desktop/pontoquiosquev2.git && pwd && git fetch --all
+cd ${HOME}/repos/pmm-system/contaspublicas.git && pwd && git fetch --all
+cd ${HOME}/repos/pmm-system/gestaocontaspublicas.git && pwd && git fetch --all
+cd ${HOME}/repos/pmm-system/habitacao.git && pwd && git fetch --all
+#cd ${HOME}/repos/pmm-system/pontoquiosquev2.git && pwd && git fetch --all
 
