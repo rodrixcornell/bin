6024fc52181d8456395e127461d75beb9092f94f
Rodrigo Cabral <rodrixcornell@gmail.com>
2016-12-18 17:34:41 -0400

    Thupan SFTP Config teste update thupan ;

--- thupan_sftp.sh
+++ thupan_sftp.sh
@@ -2,4 +2,12 @@
 
 cd $(pwd)
 
+echo $(
+    if [[ $(__git_ps1) =~ \*\)$ ]]
+    then echo "'$LIGHT_RED'"$(__git_ps1 "(%s) ")
+    elif [[ $(__git_ps1) =~ \+\)$ ]]
+    then echo "'$LIGHT_YELLOW'"$(__git_ps1 "(%s) ")
+    else echo "'$LIGHT_GREEN'"$(__git_ps1 "(%s) ")
+    fi)
+
 echo $(__git_ps1 "%s")
