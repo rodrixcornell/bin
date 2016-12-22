2caae910994e8c61c266afdfac8d428e8ff32df5
Rodrigo Cabral <rodrixcornell@gmail.com>
2016-12-22 00:07:39 -0400

    Meu IP
    Signed-off-by: Rodrigo Cabral <rodrixcornell@gmail.com>

--- my_ip.sh
+++ my_ip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #echo $(ifconfig | grep -i bcast | cut -d" " -f13)
-
-echo $(ifconfig | grep -i bcast | sed 's/          //' | cut -d' ' -f3)
+#echo $(ifconfig | grep -i bcast | sed 's/          //' | cut -d' ' -f3)
+echo $(ifconfig | grep -i bcast | column -tx | cut -d' ' -f5)
