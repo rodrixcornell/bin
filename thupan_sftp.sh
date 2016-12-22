1aa65834d060cf549fb9334815e504fea5ab5cda
Rodrigo Cabral <rodrixcornell@gmail.com>
2016-12-21 23:45:05 -0400

    agora vai...
    Signed-off-by: Rodrigo Cabral <rodrixcornell@gmail.com>

--- thupan_sftp.sh
+++ thupan_sftp.sh
@@ -2,16 +2,16 @@
 
 cd $(pwd)
 
-FRAMEWORK=$(cat .git/config |grep -i :thupan | cut -d/ -f2 | cut -d. -f1)
-
+FRAMEWORK=$([[ $(cat .git/config 2>&- | grep -i :thupan) ]] && echo thupan || echo vo)
+PROJECT=$(cat .git/config 2>&- | grep -i :thupan | cut -d/ -f2 | cut -d. -f1)
 GIT_BRANCH=$(git branch -a 2>&- | grep "*" | sed -e "s/* //")
 
-if [[ ${FRAMEWORK} ]]; then
+if [[ ${PROJECT} ]]; then
 	#statements
 	if [[ ${GIT_BRANCH} ]]; then
 		#statements
 		echo ${GIT_BRANCH}
-		echo $(cat .git/config | grep -i :thupan | cut -d/ -f 2)
+		echo ${PROJECT}
 		echo $(cat .git/config | grep -i ${GIT_BRANCH} | grep -i fetch | column -tx | cut -d/ -f5)
 	fi
 fi
