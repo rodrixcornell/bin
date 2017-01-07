ebfcfc120f551cee8977dfce292c222ed1c0429f
Rodrigo Cabral <rodrixcornell@gmail.com>
2017-01-07 18:25:30 -0400

    Projeto Git Fetch com SFTP
    Atualiza repositórios espelhados, atualiza redmine repositórios e sftp nos servidores de PRD, HOM e DEV

---
+++ git_fetch_sftp.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+cd ~
+c=0
+while true; do
+	echo
+	echo "Quantidade: "$c" "$(date +%Y%m%d.%H%M%S.%N)
+	echo -e "Git Fetch #1 $(date +%Y%m%d.%H%M%S.%N)\r"
+	for i in $(cd ~ | ls -r */*.git | grep -i : | sed "s/://"); do echo $(pwd)/$i && cd $(pwd)/$i && git fetch --all 2>&1 && cd - >/dev/null 2>&1; done
+	echo -e "Git Fetch #2 $(date +%Y%m%d.%H%M%S.%N)\r"
+	for i in $(cd ~ | ls -r */*/*.git | grep -i : | sed "s/://"); do echo $(pwd)/$i && cd $(pwd)/$i && git fetch --all 2>&1 && cd - >/dev/null 2>&1; done
+	echo -e "SFTP Carius Repos $(date +%Y%m%d.%H%M%S.%N)\r"
+	expect -c 'set timeout 3
+	spawn ssh carius -l deploy; expect "password:"; send "deploy\r"; expect "~$\r"; send "rm -rfv repos/* && mkdir -p repos\r"; expect "~$\r"; send "exit\r"; interact
+	spawn sftp -prC deploy@carius; expect "password:"; send "deploy\r" ;expect "sftp>" ;send "put -r repos/\r"; send "bye\r"; interact'
+	curl http://redmine.dsti.manaus.am.gov.br/sys/fetch_changesets?key=8aMjkoLLk40jTBaSTOai
+
+	echo -e "SFTP Carius Repos $(date +%Y%m%d.%H%M%S.%N)\r"
+	expect -c 'set timeout 3
+	spawn ssh pericuma -l deploy; expect "password:"; send "deploy\r"; expect "~$\r"; send "rm -rfv repos/* && mkdir -p repos\r"; expect "~$\r"; send "exit\r"; interact
+	spawn sftp -prC deploy@pericuma; expect "password:"; send "deploy\r" ;expect "sftp>" ;send "put -r repos/\r"; send "bye\r"; interact'
+	curl http://pericuma.manaus.am.gov.br/sys/fetch_changesets?key=8aMjkoLLk40jTBaSTOai
+
+	echo -e "SFTP Carius Repos $(date +%Y%m%d.%H%M%S.%N)\r"
+	expect -c 'set timeout 3
+	spawn ssh parnamirim -l deploy; expect "password:"; send "deploy\r"; expect "~$\r"; send "rm -rfv repos/* && mkdir -p repos\r"; expect "~$\r"; send "exit\r"; interact
+	spawn sftp -prC deploy@parnamirim; expect "password:"; send "deploy\r" ;expect "sftp>" ;send "put -r repos/\r"; send "bye\r"; interact'
+	curl http://parnamirim.manaus.am.gov.br/sys/fetch_changesets?key=8aMjkoLLk40jTBaSTOai
+
+	#git_sftp.exp >/dev/null 2>&1
+	echo "Sleep Time: "$c" "$(date +%Y%m%d.%H%M%S.%N)
+	[[ "$c" -lt 60 ]] && sleep 30 || break
+	((c++))
+done
