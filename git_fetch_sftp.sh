5b2f79df1cdf061368504ddf3b37bdeed5707445
Rodrigo Cabral <rodrixcornell@gmail.com>
2017-01-25 17:34:03 -0400

    Melhorias
    Signed-off-by: Rodrigo Cabral <rodrixcornell@gmail.com>

--- git_fetch_sftp.sh
+++ git_fetch_sftp.sh
@@ -31,6 +31,6 @@ while true; do
 
 	#git_sftp.exp >/dev/null 2>&1
 	echo "Sleep Time: "$c" "$(date +%Y%m%d.%H%M%S.%N)
-	[[ "$c" -lt 60 ]] && sleep 45 || break
+	[[ "$c" -lt 44 ]] && sleep 45 || break
 	((c++))
 done
