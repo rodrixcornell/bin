e191e52c40962096046d4d995cdff3e921aa6c16
Rodrigo Cabral <rodrixcornell@gmail.com>
2017-06-02 09:10:36 -0400

    Melhorias novos códigos no padrão do tecloado

--- minikeyboard.sh
+++ minikeyboard.sh
@@ -4,3 +4,7 @@ echo "Mini Keyboard"
 xmodmap -e "remove lock = Caps_Lock"
 xmodmap -e "keycode 66 = Alt_L"
 
+xmodmap -e "remove lock = Num_Lock"
+xmodmap -e "keycode 77 = Caps_Lock"
+
+# keycode 77 = Num_Lock
