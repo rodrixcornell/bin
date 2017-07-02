f3cfbf54b30553ef312ef9b068ce942b8d10e9b0
Rodrigo Cabral <rodrixcornell@gmail.com>
2017-07-02 13:52:38 -0400

    Atualizações
    Mini Keyboard
    
    Signed-off-by: Rodrigo Cabral <rodrixcornell@gmail.com>

--- minikeyboard.sh
+++ minikeyboard.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-#
-# keycode 66 = Caps_Lock
-# keycode 64 = Alt_L
-# keycode 77 = Num_Lock
 
 echo "Mini Keyboard"
-#keycode 77 = Num_Lock
-#keycode 66 = Caps_Lock
-#keycode 50 = Shift_L
-#keycode 37 = Control_L
-#keycode 133 = Super_L
+
+###
+# keycode 64 = Alt_L
+# keycode 77 = Num_Lock
+# keycode 66 = Caps_Lock
+# keycode 50 = Shift_L
+# keycode 37 = Control_L
+# keycode 133 = Super_L
 
 xmodmap -e "keycode 37 = Control_L"
 xmodmap -e "remove lock = Control_L"
