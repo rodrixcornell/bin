1d3dd02a3770483ed656ebaab9f131e3b059dd09
Rodrigo Cabral <rodrixcornell@gmail.com>
2018-05-09 22:33:33 -0400

    No ramo master Mudanças a serem submetidas: 	modified:   chaveiro.sh 	modified:   my_ip.sh

--- my_ip.sh
+++ my_ip.sh
@@ -2,4 +2,4 @@
 
 #echo $(ifconfig | grep -i bcast | cut -d" " -f13)
 #echo $(ifconfig | grep -i bcast | sed 's/          //' | cut -d' ' -f3)
-echo $(ifconfig | grep -i bcast | column -tx | cut -d' ' -f5) #- $(ifconfig | grep -i P-a-P | column -tx | cut -d' ' -f5)
+echo $(ifconfig | grep -i bcast | column -tx | cut -d' ' -f5) | sed 's/\ /\ -\ /g' #- $(ifconfig | grep -i P-a-P | column -tx | cut -d' ' -f5)
