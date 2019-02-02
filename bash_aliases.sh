33ffd290fb957582d9b62bc3cab0082a5a174c11
Rodrigo Cabral <rodrigo.cabral@pmm.am.gov.br>
2019-02-02 11:16:18 -0400

    No ramo develop
    Mudanças a serem submetidas:
    	modified:   bash_aliases.sh
    
    Signed-off-by: Rodrigo Cabral <rodrigo.cabral@pmm.am.gov.br>

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -150,9 +150,9 @@ alias gitkraken='gitkraken -np . '
 #alias pull_push='git pull; git push'
 #complete -o bashdefault -o default -o nospace -F _git
 
-alias sublime='subl -np . '
-alias mscode='code --log debug --disable-gpu -na . '
-alias vscode='code --log debug --disable-gpu -na . '
+alias sublime='sublime_text -np . '
+alias mscode='code --log debug --disable-gpu -nra . '
+alias vscode='code --log debug --disable-gpu -nra . '
 
 alias rar_m5='rar a -m5 -idp -y'
 alias chrome-beta-incognito-proxy='google-chrome-beta --incognito --proxy-server="http=172.19.10.1:3128"'
