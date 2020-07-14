053048f01ec4fc21868423dc3dd461d74ee40d05
Rodrigo Cabral <rodrixcornell@gmail.com>
2020-07-13 22:19:33 -0400

    No ramo master
    - Mudanças a serem submetidas:
    	modified:   backup-mysql.sh
    	new file:   db_converter.py
    
    @rodrixcornell Rodrigo Cabral <rodrixcornell@gmail.com>

--- backup-mysql.sh
+++ backup-mysql.sh
@@ -11,6 +11,7 @@ dia=`date +%Y%m%d`
 #echo $deleteOldfiles
 
 hosts='cubatao divisa jaragua ararangua'
+hosts='jaragua'
 for host in $hosts; do
   mkdir -p ${targetDir}/${host}
   # mysql --host=cubatao --port=3306 --user=root --password=4dm1n53m4d -e 'show databases' > /tmp/${databaseFilename}
@@ -27,6 +28,18 @@ for host in $hosts; do
     echo Backup Atual: $banco
     mysqldump --host=${host} --port=3306 --user=root --password=4dm1n53m4d --protocol=tcp --default-character-set=utf8 --single-transaction=TRUE --routines --events --comments --add-drop-database --add-drop-trigger --no-data ${banco} > ${targetDir}/${host}/${banco}-${dia}-00-structure.sql
     mysqldump --host=${host} --port=3306 --user=root --password=4dm1n53m4d --protocol=tcp --default-character-set=utf8 --single-transaction=TRUE --routines --events --comments --no-create-info=TRUE ${banco} > ${targetDir}/${host}/${banco}-${dia}-01-data.sql
+    ###
+    # https://www.digitalocean.com/community/tutorials/how-to-migrate-mysql-database-to-postgres-using-pgloader
+    # https://github.com/lanyrd/mysql-postgresql-converter
+    #
+    echo Backup Atual Compatible PostgreSQL: $banco
+    mysqldump --host=${host} --port=3306 --user=root --password=4dm1n53m4d --protocol=tcp --compatible=postgresql --default-character-set=utf8 --single-transaction=TRUE --routines --events --comments --add-drop-database --add-drop-trigger --no-data ${banco} > ${targetDir}/${host}/${banco}-${dia}-00-structure-pg0.sql
+    mysqldump --host=${host} --port=3306 --user=root --password=4dm1n53m4d --protocol=tcp --compatible=postgresql --default-character-set=utf8 --single-transaction=TRUE --routines --events --comments --no-create-info=TRUE ${banco} > ${targetDir}/${host}/${banco}-${dia}-01-data-pg0.sql
+    python ~/bin/db_converter.py ${targetDir}/${host}/${banco}-${dia}-00-structure-pg0.sql ${targetDir}/${host}/${banco}-${dia}-00-structure-pg.sql
+    python ~/bin/db_converter.py ${targetDir}/${host}/${banco}-${dia}-01-data-pg0.sql ${targetDir}/${host}/${banco}-${dia}-01-data-pg.sql
+    rm -rf ${targetDir}/${host}/${banco}-${dia}-00-structure-pg0.sql
+    rm -rf ${targetDir}/${host}/${banco}-${dia}-01-data-pg0.sql
+
   #  tar czvf ${targetDir}/${host}-${banco}-${dia}.tgz ${targetDir}/${host}-${banco}-${dia}.sql
     gzip -v9f ${targetDir}/${host}/${banco}-${dia}*.sql
   #  rm -f ${targetDir}/${host}-${banco}-${dia}.sql
